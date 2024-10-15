5fc0ef1a216ec9568fe2a372ced69005f85f4ed3
Anncarl Mwendwa <anncarlmwendwa@Anncarls-MacBook-Pro.local>
2024-10-15 17:55:39 +0300

    feat: page to review the trip

---
+++ app/create-trip/review_trip.jsx
@@ -0,0 +1,66 @@
+import { View, Text, StyleSheet } from "react-native";
+import React, { useContext, useEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { CreateTripContext } from "../../context/CreateTripContext";
+import DetailsCard from "../../components/createTrip/DetailsCard";
+import ButtonFull from "../../components/buttons/ButtonFull";
+import moment from "moment";
+
+export default function ReviewTrip() {
+  const navigation = useNavigation();
+
+  const { tripData, setTripData } = useContext(CreateTripContext);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: true,
+      headerBackTitleVisible: false,
+      headerTitle: "",
+      headerTransparent: true,
+    });
+  }, []);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Review your Trip</Text>
+      <Text style={styles.miniheading}>
+        Please review your trip details before proceeding
+      </Text>
+      <DetailsCard icon={"📍"} title={"Destination"}>
+        {tripData?.locationInfo?.name}
+      </DetailsCard>
+      <DetailsCard icon={"👫"} title={"Who is Travelling"}>
+        {tripData?.traveller}
+      </DetailsCard>
+      <DetailsCard icon={"📅"} title={"Travel Date"}>
+        {moment(tripData?.startDate).format("DD MMM") +
+          " - " +
+          moment(tripData?.endDate).format("DD MMM") +
+          "  "}
+        ({tripData?.totalDays + " days"})
+      </DetailsCard>
+      <DetailsCard icon={"🤑"} title={"Budget"}>
+        {tripData?.budget}
+      </DetailsCard>
+      <ButtonFull style={{ marginTop: 40 }}>Build my Trip</ButtonFull>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 20,
+    paddingTop: "25%",
+  },
+  heading: {
+    fontSize: 30,
+    fontFamily: "bold",
+    marginVertical: 20,
+  },
+  miniheading: {
+    fontSize: 18,
+    fontFamily: "medium",
+  },
+});
