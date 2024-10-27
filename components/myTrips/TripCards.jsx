1804b9267b4e0d42c56954d4d1864f40cef816fb
Anncarl Mwendwa <mwendwaanncarl@gmail.com>
2024-10-27 13:38:35 +0300

    card with summarised trip details

---
+++ components/myTrips/TripCards.jsx
@@ -0,0 +1,53 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Image } from "react-native";
+import moment from "moment";
+import { Colors } from "../../constants/Colors";
+
+export default function TripCards({ trip }) {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${trip.tripData?.locationInfo?.photo}&key=${process.env.EXPO_PUBLIC_GOOGLE_MAP_KEY}`,
+        }}
+        style={styles.image}
+      />
+      <View style={styles.textConatiner}>
+        <Text style={styles.heading}>{trip.tripPlan?.trip?.destination}</Text>
+        <Text style={styles.text}>
+          {moment(trip.tripData?.startDate).format("DD MMM YYYY")}
+        </Text>
+        <Text style={styles.text}>
+          Travelling: {trip.tripPlan?.trip?.traveler}
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 10,
+    flexDirection: "row",
+    gap: 10,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+  },
+  textConatiner: {
+    marginTop: 15,
+  },
+  heading: {
+    fontFamily: "medium",
+    fontSize: 16,
+  },
+  text: {
+    fontFamily: "regular",
+    fontSize: 14,
+    color: "#333",
+  },
+});
