f762725bcbfc5b78296c188a643dd08d8de6745c
Anncarl Mwendwa <mwendwaanncarl@gmail.com>
2025-01-23 15:24:04 +0300

    modified to mantain sessions

--- configs/FirebaseConfig.js
+++ configs/FirebaseConfig.js
@@ -1,13 +1,42 @@
-// Import the functions you need from the SDKs you need
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import {
+//   getAuth,
+//   getReactNativePersistence,
+//   initializeAuth,
+// } from "firebase/auth";
+// import AsyncStorage from "@react-native-async-storage/async-storage";
+// import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+// import { getFirestore } from "firebase/firestore";
+
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyChuIqow8h8LfMDEecCBa5iyD9tw5EKKPc",
+//   authDomain: "ai-travel-app-1ed8b.firebaseapp.com",
+//   projectId: "ai-travel-app-1ed8b",
+//   storageBucket: "ai-travel-app-1ed8b.appspot.com",
+//   messagingSenderId: "150489861460",
+//   appId: "1:150489861460:web:5c3f48400868e1802d8cc1",
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+// // export const auth = getAuth(app, {
+// //   persistence: getReactNativePersistence(AsyncStorage),
+// // });
+// export const auth = initializeAuth(app, {
+//   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+// });
+// export const db = getFirestore(app);
+
 import { initializeApp } from "firebase/app";
-import { getAuth, getReactNativePersistence } from "firebase/auth";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyChuIqow8h8LfMDEecCBa5iyD9tw5EKKPc",
   authDomain: "ai-travel-app-1ed8b.firebaseapp.com",
@@ -17,9 +46,15 @@ const firebaseConfig = {
   appId: "1:150489861460:web:5c3f48400868e1802d8cc1",
 };
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage),
+// Initialize Firebase app
+const app = initializeApp(firebaseConfig);
+
+// Initialize Auth with React Native Persistence
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
-export const db = getFirestore(app);
+
+// Initialize Firestore
+const db = getFirestore(app);
+
+export { app, auth, db };
